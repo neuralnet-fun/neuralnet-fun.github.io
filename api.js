3d2b875401e9dda8e99116b6c19e88b812e35d7d
Kay Software <neuro@kay-software.ru>
2022-08-14 17:43:18 +0300

    -

--- api.js
+++ api.js
@@ -4,8 +4,26 @@
 */
 
 const RemoteHost = ["https://kay-software.ru/neuro/"];
-(async function main() {
+
+(function OnLoad() {
+    console.log(`\"host\":\"${RemoteHost[0]}\"`)
+    Generate();
+})();
+
+async function Generate() {
+    let CopyButton = document.getElementById("copy")
+    if (CopyButton) {
+        CopyButton.innerHTML = "Копировать";
+    }
+    function Public(ClientResponse) {
+        let El = document.getElementById("de-generator-result");
+        if (El) {El.innerHTML = ClientResponse;}
+        console.log(`\"response\":\"${ClientResponse}\"`);
+        return true;
+    }
+    Public("<span style=\"color: green;\">Запрос к API...</span>");
     function GetDatabaseValue(section) {
+        console.log(`\"request\":\"${section}\"`);
         return RemoteHost[0] + ("generated/seed." + section + ".txt");
     }
     function PsRand(a, b) {
@@ -14,18 +32,13 @@ const RemoteHost = ["https://kay-software.ru/neuro/"];
     let api_request = await fetch(GetDatabaseValue("api"));
     let api = await api_request.text();
     let api_arr = api.split("|");
+    console.log(`\"name\":\"${api_arr[0]}\"`);
+    console.log(`\"version\":\"${api_arr[1]}\"`);
+    let seed = PsRand(api_arr[2], api_arr[3]);
     try {
-        function Public(HostResponse) {
-            let El = document.getElementById("de-generator-result");
-            El.innerHTML = HostResponse;
-            console.log(`\"response\":\"${HostResponse}\"`)
-            return true;
-        }
-        let Seed = PsRand(api_arr[2], api_arr[3]);
-        console.log(`\"seed\":${Seed}`)
-        await fetch(GetDatabaseValue(Seed)).then(response => response.text()).then(code => Public(code));
+        await fetch(GetDatabaseValue(seed)).then(response => response.text()).then(code => Public(code));
     } catch (e) {
         Public("Ваш браузер не поддерживает используемые стандарты страницы :(");
     }
     return true;
-})();
+};
