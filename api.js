a9d5373d8c891d5b30661bd0ea11a2a2b6641c3f
Kay Software <neuro@kay-software.ru>
2022-08-15 00:01:44 +0300

    -

--- api.js
+++ api.js
@@ -5,41 +5,57 @@
 
 const RemoteHost = ["https://kay-software.ru/neuro/"];
 
+const LogsDate = new Date();
+
+let counter = 0;
+
 (function OnLoad() {
-    console.log(`\"host\":\"${RemoteHost[0]}\"`)
+    FixTheTime("begin");
     Generate();
 })();
 
+function FixTheTime(OperationName) {
+    console.log(`\"${OperationName}\":\"${LogsDate.getHours()}:${LogsDate.getMinutes()}:${LogsDate.getMilliseconds()}\"`);
+}
+
 async function Generate() {
-    let CopyButton = document.getElementById("copy");
-    if (CopyButton) {
-        CopyButton.innerHTML = "Копировать";
-    }
-    function Public(ClientResponse) {
-        let El = document.getElementById("de-generator-result");
-        if (El) {El.innerHTML = ClientResponse;}
-        console.log(`\"response\":\"${ClientResponse}\"`);
-        return true;
-    }
-    Public("<span style=\"color: green;\">Запрос к API...</span>");
-    function GetDatabaseValue(section) {
-        console.log(`\"request\":\"${section}\"`);
-        return RemoteHost[0] + ("generated/seed." + section + ".txt");
-    }
-    function PsRand(a, b) {
-        return Number(a) + Number(Math.floor(Math.random() * b));
-    }
-    let api_request = await fetch(GetDatabaseValue("api"));
-    let api = await api_request.text();
-    let api_arr = api.split("|");
-    console.log(`\"name\":\"${api_arr[0]}\"`);
-    console.log(`\"version\":\"${api_arr[1]}\"`);
-    let seed = PsRand(api_arr[2], api_arr[3]);
     try {
+        counter++; console.log(`\"count\":\"${counter}\"`);
+        FixTheTime(`begin_task:${counter}`);
+        let CopyButton = document.getElementById("copy");
+        if (CopyButton) {
+            CopyButton.innerHTML = "Копировать";
+        }
+        function Public(ClientResponse) {
+            if (!ClientResponse.includes("span")) {
+                let El = document.getElementById("de-generator-result");
+                if (El) {El.innerHTML = ClientResponse;}
+                console.log(`\"response\":\"${ClientResponse}\"`);
+            }
+            return true;
+        }
+        Public("<span style=\"color: green;\">Запрос к API...</span>");
+        function GetDatabaseValue(section) {
+            FixTheTime("format_begin");
+            console.log(`\"request\":\"${section}\"`);
+            FixTheTime("format_end");
+            return RemoteHost[0] + ("generated/seed." + section + ".txt");
+        }
+        function PsRand(a, b) {
+            return Number(a) + Number(Math.floor(Math.random() * b));
+        }
+        let api_request = await fetch(GetDatabaseValue("api"));
+        let api = await api_request.text();
+        let api_arr = api.split("|");
+        console.log(`\"name\":\"${api_arr[0]}\"`);
+        console.log(`\"version\":\"${api_arr[1]}\"`);
+        let seed = PsRand(api_arr[2], api_arr[3]);
         await fetch(GetDatabaseValue(seed)).then(response => response.text()).then(code => Public(code));
     } catch (e) {
-        Public("Ваш браузер не поддерживает используемые стандарты страницы :(");
+        Public(`<span style=\"color: red;\"><pre>${e}</pre></span>`);
         return false;
     }
+    FixTheTime(`end_task:${counter}`);
+    console.log(`---`);
     return true;
 };
